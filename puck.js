4afa44a6bbaad985ff42ff8e7e649206a57e2293
Lin Cao <131940517+Lilycomeon@users.noreply.github.com>
2024-02-29 13:52:26 +0100

    Add files via upload

---
+++ puck.js
@@ -0,0 +1,86 @@
+class Puck
+{
+  constructor()
+  {
+    this.reset();
+    this.radius = 10;
+  }
+  
+  reset()
+  {
+    this.x = width/2;
+    this.y = height/2;
+    this.xSpeed = 4;
+    this.ySpeed = 4;
+    
+    if(random(1) > 0.5)
+    {
+      this.xSpeed *= -1;
+    }
+    
+    if(random(1) > 0.5)
+    {
+      this.ySpeed *= -1;
+    }
+  }
+  
+  move()
+  {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    
+    this.bounceEdges();
+    this.checkGoals();
+  }
+  
+  checkCollision(paddle)
+  {
+    var puckLeft = this.x - this.radius;
+    var puckRight = this.x + this.radius;
+    var puckButtom = this.y + this.radius;
+    var puckTop = this.y - this.radius;
+    
+    var paddleLeft = paddle.x - paddle.w/2;
+    var paddleRight = paddle.x + paddle.w/2;
+    var paddleTop = paddle.y - paddle.h/2;
+    var paddleBottom = paddle.y + paddle.h/2;
+    
+    if(puckLeft < paddleRight &&
+      puckRight > paddleLeft &&
+      puckTop < paddleBottom &&
+      puckButtom > paddleTop)
+    {
+      this.xSpeed *= -1;
+    }
+  }
+  
+  bounceEdges()
+  {
+    if (this.y + this.radius > height || 
+        this.y - this.radius < 0)
+    {
+     this.ySpeed *= -1;  
+    }
+  }
+  
+  checkGoals()
+  {
+    if(this.x + this.radius > width)
+    {
+      this.reset();
+      leftScore++;
+    }
+    
+    if(this.x + this.radius < 0)
+    {
+      this.reset();
+      rightScore++;
+    }
+  }
+  
+  draw()
+  {
+    fill(0);
+    circle(this.x, this.y, this.radius*2)
+  }
+}
